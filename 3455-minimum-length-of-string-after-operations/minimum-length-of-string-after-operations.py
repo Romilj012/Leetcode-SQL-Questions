4a32c7ff9ddaa5d1b7d9f4e0b14282802939e250
Romil Jain <40470068+Romilj012@users.noreply.github.com>
2025-01-13 22:16:23 -0600

    Time: 138 ms (55.08%) | Memory: 18.6 MB (63.93%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,14 +1,22 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        c = Counter()
         res = 0
-        for i in s:
-            c[i]+= 1
-        for i, k in c.items():
-            if k % 2 == 0:
+        for count in Counter(s).values():
+            if count % 2 == 0:
                 res+=2
             else:
                 res +=1
-        return res
+        return res            
+        # c = Counter()
+        # res = 0
+        # for i in s:
+        #     c[i]+= 1
+        # for i, k in c.items():
+        #     if k % 2 == 0:
+        #         res+=2
+        #     else:
+        #         res +=1
+        # return res
+        
 
         
